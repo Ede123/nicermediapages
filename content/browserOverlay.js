72217dd5903db9d9785a55be56cc7a130fe36fa2
Eduard Braun <Eduard.Braun2@gmx.de>
2014-09-08 06:54:04 +0200

    Adjust position/size/scaling of transparency layer for SVGs whose dimensions are manipulated using the "viewBox" attribute

--- content/browserOverlay.js
+++ content/browserOverlay.js
@@ -13,20 +13,27 @@ var nicermediapages = (function() {
 		svg.setAttribute("id", "nicermediapages-SVG");
 		var viewBox = svg.viewBox;
 		
-		// add a "foreignObject" with 100% width/height to the bottom of the SVG
+		// add a "foreignObject" element with 100% width/height to the bottom of the SVG
 		// to the contained "body" element we can apply all necessary customizations
 		var rect = doc.createElementNS("http://www.w3.org/2000/svg", "foreignObject");
-		rect.setAttribute("width", "100%");
-		rect.setAttribute("height", "100%");
-		if (viewBox && viewBox.baseVal) {
-			rect.setAttribute("x", viewBox.baseVal.x);
-			rect.setAttribute("y", viewBox.baseVal.y);
-		}
+		rect.setAttribute("width", svg.width.baseVal.valueAsString);
+		rect.setAttribute("height", svg.height.baseVal.valueAsString);
 		var body = doc.createElementNS("http://www.w3.org/1999/xhtml", "body");
 		body.setAttribute("id", "nicermediapages-body");
+		// adjust position and scaling of the "foreignObject" if the SVG's size is manipulated using the viewBox attribute
+		if (viewBox && viewBox.baseVal) {
+			var mx = viewBox.baseVal.width / svg.width.baseVal.value;
+			var my = viewBox.baseVal.height / svg.height.baseVal.value;
+			var m = Math.max(mx,my);
+			rect.setAttribute("x", (viewBox.baseVal.width/2 + viewBox.baseVal.x) / m - svg.width.baseVal.value/2);
+			rect.setAttribute("y", (viewBox.baseVal.height/2 + viewBox.baseVal.y) / m - svg.height.baseVal.value/2);
+			rect.setAttribute("transform", "scale(" + m + ")");
+		}
+		// add the elements
 		rect.appendChild(body);
 		svg.insertBefore(rect, svg.firstChild);
 		
+		// event listeners to implement transparency on hover
 		svg.addEventListener("mouseover", mouseEvent, true);
 		svg.addEventListener("mouseout", mouseEvent, true);
 	};
