c7ef9c1c84548744ba56e0164c0ea1352f3c8c6d
Eduard Braun <Eduard.Braun2@gmx.de>
2014-09-16 23:16:20 +0200

    compatibility fix for Firefox < 17 interface "nsIMessageListenerManager" was called "nsIChromeFrameMessageManager" back then

--- content/browserOverlay.js
+++ content/browserOverlay.js
@@ -1,2 +1,7 @@
-var globalMM = Cc["@mozilla.org/globalmessagemanager;1"].getService(Ci.nsIMessageListenerManager);
+var globalMM;
+if (Services.vc.compare(Services.appinfo.platformVersion, "16.*") <= 0) // Interface was renamed starting with Firefox 17
+	globalMM = Cc["@mozilla.org/globalmessagemanager;1"].getService(Ci.nsIChromeFrameMessageManager);
+else
+	globalMM = Cc["@mozilla.org/globalmessagemanager;1"].getService(Ci.nsIMessageListenerManager);
+
 globalMM.loadFrameScript("chrome://nicermediapages/content/nicermediapages.js", true);
