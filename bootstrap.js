181e57f268450371d942e75605bd084c277e9836
Eduard Braun <Eduard.Braun2@gmx.de>
2014-10-19 20:32:29 +0200

    Make add-on restartless

---
+++ bootstrap.js
@@ -0,0 +1,51 @@
+const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+Cu.import("resource://gre/modules/Services.jsm");
+
+// store the current time (it will be appended to resource URIs via "?" to work around caching issues)
+var startupTime = Date.now();
+
+
+/* Bootstrap entry points */
+function startup(data,reason) {
+	// register style sheets
+	var sss = Cc["@mozilla.org/content/style-sheet-service;1"].getService(Ci.nsIStyleSheetService);
+	var ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+	var uri = ios.newURI("chrome://nicermediapages/skin/TopLevelSVGDocument.css?" + startupTime, null, null);
+	if (!sss.sheetRegistered(uri, sss.USER_SHEET)) {
+		sss.loadAndRegisterSheet(uri, sss.USER_SHEET);
+	}
+
+	// register frame script in global message manager (e10s compatibility; gets loaded into every <browser> in every chrome window)
+	var globalMM;
+	if (Services.vc.compare(Services.appinfo.platformVersion, "16.*") <= 0) // Interface was renamed starting with Firefox 17
+		globalMM = Cc["@mozilla.org/globalmessagemanager;1"].getService(Ci.nsIChromeFrameMessageManager);
+	else
+		globalMM = Cc["@mozilla.org/globalmessagemanager;1"].getService(Ci.nsIMessageListenerManager);
+	globalMM.loadFrameScript("chrome://nicermediapages/content/nicermediapages.js?" + startupTime, true);
+}
+
+function shutdown(data,reason) {
+	// no unloading needed on Firefox shutdown
+	if (reason == APP_SHUTDOWN)
+		return;
+	
+	// unregister style sheets
+	var sss = Cc["@mozilla.org/content/style-sheet-service;1"].getService(Ci.nsIStyleSheetService);
+	var ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+	var uri = ios.newURI("chrome://nicermediapages/skin/TopLevelSVGDocument.css?" + startupTime, null, null);
+	if (sss.sheetRegistered(uri, sss.USER_SHEET)) {
+		sss.unregisterSheet(uri, sss.USER_SHEET);
+	}
+	
+	// unregister frame script in global message manager
+	var globalMM;
+	if (Services.vc.compare(Services.appinfo.platformVersion, "16.*") <= 0) // Interface was renamed starting with Firefox 17
+		globalMM = Cc["@mozilla.org/globalmessagemanager;1"].getService(Ci.nsIChromeFrameMessageManager);
+	else
+		globalMM = Cc["@mozilla.org/globalmessagemanager;1"].getService(Ci.nsIMessageListenerManager);
+	globalMM.removeDelayedFrameScript("chrome://nicermediapages/content/nicermediapages.js?" + startupTime);
+}
+
+function install(data, reason) {}
+
+function uninstall(data, reason) {}
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 rm nicermediapages.xpi
-7z a -mx -mtc- -r -i!install.rdf -i!chrome.manifest -i!content/ -i!skin/ nicermediapages.xpi
+7z a -mx -mtc- -r -i!install.rdf -i!chrome.manifest -i!bootstrap.js -i!content/ -i!skin/ nicermediapages.xpi
