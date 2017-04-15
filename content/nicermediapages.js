c48ebb4780d0b5d6f64feca6eabbf5f5daa9801b
ExE Boss <ExE-Boss@users.noreply.github.com>
2017-04-15 18:43:39 +0200

    Convert to a WebExtension
    Signed-off-by: ExE Boss <ExE-Boss@users.noreply.github.com>

--- content/nicermediapages.js
+++ content/nicermediapages.js
@@ -1,18 +1,16 @@
 var nicermediapages = (function() {
 
-	initSVG = function() {
-		var doc = content.document;
-
+	function initSVG() {
 		// get SVG root element
-		var svg = doc.documentElement;
+		var svg = document.documentElement;
 		var viewBox = svg.viewBox;
 
 		// add a "foreignObject" element with 100% width/height to the bottom of the SVG
 		// to the contained "body" element we can apply all necessary customizations
-		var rect = doc.createElementNS("http://www.w3.org/2000/svg", "foreignObject");
+		var rect = document.createElementNS("http://www.w3.org/2000/svg", "foreignObject");
 		rect.setAttribute("width", svg.width.baseVal.value);
 		rect.setAttribute("height", svg.height.baseVal.value);
-		var body = doc.createElementNS("http://www.w3.org/1999/xhtml", "body");
+		var body = document.createElementNS("http://www.w3.org/1999/xhtml", "body");
 		// adjust position and scaling of the "foreignObject" if the SVG's size is manipulated using the viewBox attribute
 		// check for "viewBox.baseVal.width != 0" needed for compatibility with older Firefox versions < 22 (bugs 888307 and 785606)
 		if (viewBox && viewBox.baseVal && viewBox.baseVal.width !== 0) {
@@ -32,8 +30,8 @@ var nicermediapages = (function() {
 		svg.addEventListener("mouseout", mouseEvent, true);
 	};
 
-	mouseEvent = function(aEvent) {
-		var svg = content.document.getElementsByClassName("nicermediapages-SVG")[0];
+	function mouseEvent(aEvent) {
+		var svg = document.getElementsByClassName("nicermediapages-SVG")[0];
 		if (aEvent.type === "mouseover" && !aEvent.target.classList.contains("nicermediapages-SVG")) {
 			svg.classList.add("hovered");
 		}
@@ -42,41 +40,38 @@ var nicermediapages = (function() {
 		}
 	};
 
-
 	// public functions
 	return {
 		init: function() {
 			// if loaded document is an SVG/Image/Video/ document
 			// add the respective class names and do any necessary initializing
-			var doc = content.document;
-			if (doc.contentType.indexOf("image/svg") === 0) /* SVG Document */ {
-				if (doc.URL.indexOf("view-source:") === -1) { // prevent modifying "view source" page
-					doc.documentElement.classList.add("nicermediapages-SVG");
+			if (document.contentType.indexOf("image/svg") === 0) /* SVG Document */ {
+				if (document.URL.indexOf("view-source:") === -1) { // prevent modifying "view source" page
+					document.documentElement.classList.add("nicermediapages-SVG");
 					initSVG();
 				}
-			} else if (doc.contentType.indexOf("image/") === 0) /* Image Document */  {
-				doc.documentElement.classList.add("nicermediapages-Image");
-			} else if (doc.contentType.indexOf("video/") === 0) /* Video Document */  {
-				doc.documentElement.classList.add("nicermediapages-Video");
+			} else if (document.contentType.indexOf("image/") === 0) /* Image Document */  {
+				document.documentElement.classList.add("nicermediapages-Image");
+			} else if (document.contentType.indexOf("video/") === 0) /* Video Document */  {
+				document.documentElement.classList.add("nicermediapages-Video");
 			}
 		}
 	};
 
 }());
 
-
 // add an event listener that fires (and initiates the add-on) when a new document is loaded
 // (since "DOMContentLoaded" does not fire on Image/Video Documents for some reason use "readystatechange")
-addEventListener("readystatechange", function() {
+addEventListener("readystatechange", function () {
 	// initiate when readyState becomes "interactive" (document has finished parsing)
 	// ignore readyState subsequently reaching "complete" (document has finished loading)
 	//   since we would unnecessarily initiate a second time otherwise
-	if (content.document.readyState === "interactive") {
+	if (document.readyState === "interactive") {
 		nicermediapages.init();
 	}
 }, true);
 
 // initiate the add-on on already loaded pages right away
-if (content.document.readyState === "complete") {
+if (document.readyState === "complete") {
 	nicermediapages.init();
 }
